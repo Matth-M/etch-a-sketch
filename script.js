3fa5f970b1090607a872618cbc42cf90657db6a6
Matth-M <matthieu.matsima@gmail.com>
2022-07-22 17:37:01 +0200

    Change cell color on mouse hover

--- script.js
+++ script.js
@@ -13,14 +13,19 @@ function drawGrid(cellsPerSide){
 	for(let i = 0; i < numberOfCells; ++i){
 		const cell = document.createElement('div');
 		cell.classList.add('cell');
+		cell.addEventListener('mouseover', function(){
+			this.classList.toggle('colored');
+		});
 		cell.style.width = Math.floor(gridWidth / cellsPerSide) + 'px';
 		cell.style.height = Math.floor(gridHeight / cellsPerSide) + 'px';
 		container.appendChild(cell);
 	}
 }
-const cells = document.querySelectorAll('main .cell');
-cells.forEach(cell => {
-	cell.addEventListener('mouseover', function(){
-		this.classList.toggle('colored');
-	});
-});
+
+function resetGrid(){
+	while(container.hasChildNodes()){
+		container.removeChild(container.firstChild);
+	}
+}
+
+drawGrid(4); //Display a grid by default
