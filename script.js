0050081afdb869371db10d7e3e77d09a8ed5c1eb
Matth-M <matthieu.matsima@gmail.com>
2022-07-22 16:01:33 +0200

    User can select the size of the grid

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+const inputGridSize = document.querySelector('input#gridSize');
+inputGridSize.addEventListener('input', function(e){
+	const cellsPerSide = e.target.value;
+	drawGrid(cellsPerSide);
+})
+
 const container = document.querySelector('#container');
-const gridSize = 4;
-const numberOfCells = gridSize * gridSize;
-for(let i = 0; i < numberOfCells; ++i){
-	const cell = document.createElement('div');
-	cell.classList.add('cell');
-	container.appendChild(cell);
+
+function drawGrid(cellsPerSide){
+	resetGrid();
+	[gridHeight, gridWidth] = [container.clientHeight, container.clientWidth];
+	const numberOfCells = cellsPerSide * cellsPerSide;
+	for(let i = 0; i < numberOfCells; ++i){
+		const cell = document.createElement('div');
+		cell.classList.add('cell');
+		cell.style.width = Math.floor(gridWidth / cellsPerSide) + 'px';
+		cell.style.height = Math.floor(gridHeight / cellsPerSide) + 'px';
+		container.appendChild(cell);
+	}
 }
+const cells = document.querySelectorAll('main .cell');
+cells.forEach(cell => {
+	cell.addEventListener('mouseover', function(){
+		this.classList.toggle('colored');
+	});
+});
